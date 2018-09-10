20925b74a2220ca2e8e87a9e89401783602f6972
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-09-10 05:40:41 -0600

    ⏣ set up UI

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,13 +1,39 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+			// init obj for storing app data
+			let app = {
+				url: window.location.href
+			}
 
-	}
+			// build operations for uis
+			const uis = {
+				btn: () => {
+					const container = document.querySelector('#react-root > section > main'),
+						id = 'im-btn',
+						btnRef = document.getElementById(id);
+					if (container && !btnRef) {
+						let btn = document.createElement('button');
+						btn.setAttribute('id', id);
+						btn.innerText = 'Like Posts';
+						btn.style.margin = '25px auto 0px auto';
+						btn.style.width = '250px';
+						container.insertBefore(btn, container.firstChild);
+					}
+				},
+				all: () => {
+					uis.btn();
+				}
+			}
+
+			// track all clicks to inject uis
+			document.addEventListener('click', uis.all);
+
+			// init ui build when app loads
+			uis.all();
+
+		}
 	}, 10);
 });
